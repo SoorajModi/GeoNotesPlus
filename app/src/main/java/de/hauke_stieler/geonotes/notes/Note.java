f18f44fd7669fad9ff3ca0ad5d12bb27d324047e
rwiskail <rwiskail@uoguelph.ca>
2021-02-10 21:18:18 -0500

    Removed MediaNote, moved its functionality to Note class for simplification

--- app/src/main/java/de/hauke_stieler/geonotes/notes/Note.java
+++ app/src/main/java/de/hauke_stieler/geonotes/notes/Note.java
@@ -1,15 +1,27 @@
 package de.hauke_stieler.geonotes.notes;
 
+import android.net.Uri;
+
 public class Note {
+    enum MediaType {
+        NULL,
+        IMAGE,
+        AUDIO
+    }
+
     public final long id;
     public final String description;
     public final double lat;
     public final double lon;
+    public final Uri mediaURI;
+    public final MediaType mediaType;
 
-    public Note(long id, String description, double lat, double lon) {
+    public Note(long id, String description, double lat, double lon, Uri mediaURI, MediaType mediaType) {
         this.id = id;
         this.description = description;
         this.lat = lat;
         this.lon = lon;
+        this.mediaURI = mediaURI;
+        this.mediaType = mediaType;
     }
 }
