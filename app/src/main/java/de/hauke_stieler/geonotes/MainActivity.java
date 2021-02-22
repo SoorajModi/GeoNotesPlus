ed40ad6767c8d14c86674c3877708ff93913755d
SoorajModi <soomodi@gmail.com>
2021-02-22 14:13:08 -0500

    Add import i accidentally removed

--- app/src/main/java/de/hauke_stieler/geonotes/MainActivity.java
+++ app/src/main/java/de/hauke_stieler/geonotes/MainActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.os.PowerManager;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
