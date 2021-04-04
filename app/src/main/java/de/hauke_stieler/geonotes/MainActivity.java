38c2de7b5b7f1a0a6b0f0a50e718d304af99385e
Mustafa Al-Obaidi <malobaid@uoguelph.ca>
2021-04-04 22:50:37 +0000

    Update MainActivity.java

--- app/src/main/java/de/hauke_stieler/geonotes/MainActivity.java
+++ app/src/main/java/de/hauke_stieler/geonotes/MainActivity.java
@@ -330,6 +330,7 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == PICK_IMAGE) {
             //TODO: store the image in the DB
+            Toast.makeText(this, "Image have been captured succefully", Toast.LENGTH_SHORT).show();
         }
     }
 }
